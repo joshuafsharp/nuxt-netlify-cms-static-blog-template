f1ca52a330e5c21c0d44b2783d08b932516928e3
Joshua Sharp <joshuafsharp@gmail.com>
2020-10-24 00:48:21 +1100

    Update project configuration
    Rename `nuxt.config.js` to `nuxt.config.ts`.
    
    Move all the project source files to a `src` directory, and update the
    `srcDir` attribute in the nuxt.config.ts.
    
    Add a `.vscode/settings.json` file with the following workspace settings
    to force prettier to format files on save.
    
    Update prettier config.
    
    Add vue-property-decorator package & update homepage component
    definition.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">nuxt-static-blog</h1>
       <div class="links">
         <a
@@ -26,9 +25,10 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Vue } from 'vue-property-decorator';
 
-export default Vue.extend({})
+@Component
+export default class HomePage extends Vue {}
 </script>
 
 <style>
